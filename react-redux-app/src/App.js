a59941b9cb33d1cd29b14cc54554197bacd12ae4
bodhibrdr <julie.gumerman@gmail.com>
2019-09-14 19:47:27 -0600

    first step towards components

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,33 +1,41 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.scss';
 
-import { connect } from "react-redux";
-import { getTrails } from "./actions/index";
-import TrailInfo from "./components/TrailInfo"
+import BikeTrails from "./components/BikeTrails";
 
-function App({trails, getTrails, isFetching, error}) {
-  useEffect(() => {
-    getTrails();
-  }, [getTrails]);
-
+function App() {
   return (
-    <div className="App">
-      <h1>Mountain Bike Trails</h1>
-      <div className="wrapper">
-      {trails.map(trail => <TrailInfo trail={trail} />)}
-      </div>
-      <div className="footer">
-        <p>API and information therein courtesy of Mountain Bike Project</p>
-      </div>
-    </div>
-  );
-}
-const mapStateToProps = state => {
-  return {
-    trails: state.trails, 
-    isFetching: state.isFetching, 
-    error: ""
-  }
+    <BikeTrails />
+  )
 }
 
-export default connect (mapStateToProps, {getTrails})(App);
+// import { connect } from "react-redux";
+// import { getTrails } from "./actions/index";
+// import TrailInfo from "./components/TrailInfo"
+
+// function App({trails, getTrails, isFetching, error}) {
+//   useEffect(() => {
+//     getTrails();
+//   }, [getTrails]);
+
+//   return (
+//     <div className="App">
+//       <h1>Mountain Bike Trails</h1>
+//       <div className="wrapper">
+//       {trails.map(trail => <TrailInfo trail={trail} />)}
+//       </div>
+//       <div className="footer">
+//         <p>API and information therein courtesy of Mountain Bike Project</p>
+//       </div>
+//     </div>
+//   );
+// }
+// const mapStateToProps = state => {
+//   return {
+//     trails: state.trails, 
+//     isFetching: state.isFetching, 
+//     error: ""
+//   }
+// }
+export default App;
+// export default connect (mapStateToProps, {getTrails})(App);
