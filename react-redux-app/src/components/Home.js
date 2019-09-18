1e94c83e159acb14d650218ade3b888011964469
bodhibrdr <julie.gumerman@gmail.com>
2019-09-18 08:44:08 -0600

    updating the homepage, at last

--- react-redux-app/src/components/Home.js
+++ react-redux-app/src/components/Home.js
@@ -6,7 +6,10 @@ const Home = () => {
     return (
         <div>
         <Header />
+        <div className="home-screen">
         <h3>There's no place like home...especially if you live in Colorado!</h3>
+        </div>
+
         </div>
     )
 }
